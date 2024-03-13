e51a68071d56385a382f17ad395b4ae010c035c2
ValentaneVacquie28 <150685341+ValentaneVacquie28@users.noreply.github.com>
2024-03-13 09:26:14 +0100

    Update functions_consultations.php

--- GestionConsultations_API/functions_consultations.php
+++ GestionConsultations_API/functions_consultations.php
@@ -3,103 +3,9 @@
 
     require('auth_API/jwt_utils.php');
 
-    /*
-	function getAllPhrases(){
-		$resultat = $linkpdo->prepare('SELECT * FROM chuckn_facts');
-		if ($resultat == false) {
-			deliver_response('500', 'Erreur dans la préparation de la requête', $resultat);
-	    } else {
-	        $resultat->execute();
-	    	if ($resultat == false) {
-	    		deliver_response('500', 'Erreur dans l\'exécution de la requête', $resultat);
-	        } else {
-	            return $resultat;
-			}
-		}
-	}
-*/
     function getAllConsultations(){
 
     }
-
-
-
-
-/*
-	// A faire : Verifier si le id existe bien 
-	function getPhrase($id){
-		$resultat = $linkpdo->prepare('SELECT * FROM chuckn_facts WHERE id = $id');
-		if ($resultat == false) {
-			deliver_response('500', 'Erreur dans la préparation de la requête', $resultat);
-		} else {
-			$resultat->execute();
-			if ($resultat == false) {
-				deliver_response('500', 'Erreur dans l\'exécution de la requête', $resultat);
-			} else {			         
-				return $resultat;		                	
-			}
-		}
-	}
-
-	function addPhrase(){
-		$resultat = $linkpdo->prepare('INSERT INTO chuckn_facts (phrase) VALUES nouvellePhrase'); //nouvelle phrase a trouver
-		if ($resultat == false) {
-			deliver_response('500', 'Erreur dans la préparation de la requête', $resultat);
-		} else {
-			$resultat->execute();
-			if ($resultat == false) {
-			    deliver_response('500', 'Erreur dans l\'exécution de la requête', $resultat);
-			} else {
-				//................................
-			}
-		}
-	}
-
-	function majPartielPhrase($id){
-		$resultat = $linkpdo->prepare(''); ///////////A faire
-		if ($resultat == false) {
-			deliver_response('500', 'Erreur dans la préparation de la requête', $resultat);
-		} else {
-			$resultat->execute();
-			if ($resultat == false) {
-			    deliver_response('500', 'Erreur dans l\'exécution de la requête', $resultat);
-			} else {
-				//................................
-			}
-		}
-	}
-
-	function majTotalPhrase($id, $phrase, $vote, $faute, $signalement){
-		$resultat = $linkpdo->prepare(''); ///////////A faire
-		if ($resultat == false) {
-			deliver_response('500', 'Erreur dans la préparation de la requête', $resultat);
-		} else {
-			$resultat->execute();
-			if ($resultat == false) {
-			    deliver_response('500', 'Erreur dans l\'exécution de la requête', $resultat);
-			} else {
-				//................................
-			}
-		}
-	}
-
-	function deletePhrase($id){
-		$resultat = $linkpdo->prepare(''); ///////////A faire
-		if ($resultat == false) {
-			deliver_response('500', 'Erreur dans la préparation de la requête', $resultat);
-		} else {
-			$resultat->execute();
-			if ($resultat == false) {
-			    deliver_response('500', 'Erreur dans l\'exécution de la requête', $resultat);
-			} else {
-				//................................
-			}
-		}
-	}
-    */
-
-
-    
 
     function getConsultation($idConsultation){
 
@@ -114,7 +20,7 @@
     }
 
     function deleteConsultation($idConsultation){
-
+        
     }
 
 ?>
