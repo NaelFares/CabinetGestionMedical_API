3c0b454b243c78b188049d794c6cd1d62bfad650
NaelFares <frs.nael@gmail.com>
2024-03-15 09:18:55 +0100

    préparation du terrain pour usagers et connexion BD

--- Modules/connexion_db.php
+++ Modules/connexion_db.php
@@ -1,8 +1,24 @@
 <?php
+
+$server = "localhost";
+$db = "cabinetmedicalapi_bd_app";
+$login = "root";
+
+// Connexion au serveur MySQL always data
+try {
+    $linkpdo = new PDO("mysql:host=$server;dbname=$db", $login);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+//Pour la version en ligne sur alwaysdata
+
+/*
    $server = "mysql-cabinetmedicalapi.alwaysdata.net";
    $db = "cabinetmedicalapi_bd_app";
    $login = "350794";
-   $mdp = '$iutinfo'
+   $mdp = '$iutinfo';
 
    // Connexion au serveur MySQL always data
    try {
@@ -10,4 +26,5 @@
    } catch (Exception $e) {
        die('Erreur : ' . $e->getMessage());
    }
+   */
 ?>
